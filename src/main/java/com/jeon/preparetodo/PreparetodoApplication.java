7b888eb68cfbebe04bf6db10cf87a4f1a57e434e
jeonjinwoo <hp5234@naver.com>
2023-02-14 22:41:19 +0900

    프로젝트 생성

---
+++ src/main/java/com/jeon/preparetodo/PreparetodoApplication.java
@@ -0,0 +1,13 @@
+package com.jeon.preparetodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparetodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparetodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeon/preparetodo/PreparetodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeon.preparetodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparetodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
